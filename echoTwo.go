5aee5e8a673dbedc2ebf03d2c3eafdffec7dedb0
Evgeniy MacMini <evgeniy@naymushin.xyz>
2024-06-22 01:30:30 +0300

    exec

--- echoTwo.go
+++ echoTwo.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	sep, s := " ", ""
-	for _, arg := range os.Args[1:] {
+	fmt.Printf("Name of progs is %s\n", os.Args[0])
+	for i, arg := range os.Args[1:] {
+		fmt.Printf("Index of element is %d, value is %s\n", i, arg)
 		s += arg + sep
 	}
 	fmt.Println(s)
